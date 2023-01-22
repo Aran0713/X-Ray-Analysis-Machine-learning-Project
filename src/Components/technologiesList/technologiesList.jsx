f79ad0ca2a863d4ed3a046d66885cfc77262f961
kincaid1207 <73080418+kincaid1207@users.noreply.github.com>
2023-01-21 19:55:04 -0500

    edditing refresh

--- src/Components/technologiesList/technologiesList.jsx
+++ src/Components/technologiesList/technologiesList.jsx
@@ -1,29 +1,29 @@
 import "./technologiesList.css";
 import React, { useState } from "react";
 
-function ImageUpload() {
-  const [image, setImage] = useState(null);
+export default function technologiesList(props) {
+  function ImageUpload() {
+    const [image, setImage] = useState(null);
 
-  const handleChange = (event) => {
-    setImage(event.target.files[0]);
+    let handleChange = (event) => {
+      setImage(event.target.files[0]);
+    };
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let formData = new FormData();
+    formData.append("image", "image");
+
+    fetch("", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
   };
-}
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  let formData = new FormData();
-  formData.append("image", "image");
-
-  fetch("", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
-};
-
-const TechnologiesList = () => {
   return (
     <div>
       {" "}
@@ -41,20 +41,17 @@ const TechnologiesList = () => {
             The application is meant to take an image of a patient's lungs and
             detect wether the cells in their body are cancerous.
           </p>
-          <img
-            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fnews.mit.edu%2F2019%2Fmimic-chest-x-ray-database-0201&psig=AOvVaw0R-RgUhVktXiAqwdkh8o4K&ust=1674429144481000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNjs4OXk2fwCFQAAAAAdAAAAABAJ"
-            alt=""
-          />
-          ;
+          <img src="" alt="" />
         </div>
 
         <form action="">
           <label>
             {" "}
-            Upload Image: <input type="file" name="image" />
+            Upload Image:{" "}
+            <input type="file" name="image" onChange={this.handleChange} />
           </label>
           <br />
-          <button id="btn" type="submit">
+          <button id="btn" type="submit" onClick={this.handleSubmit}>
             {" "}
             Submit{" "}
           </button>
@@ -62,6 +59,4 @@ const TechnologiesList = () => {
       </div>
     </div>
   );
-};
-
-export default TechnologiesList;
+}
