3927ac4927cca26621d42bd61ccac1a98ef28b6d
Aran Abrahamlingam <aran0713@hotmail.com>
2023-01-22 05:06:26 -0500

    Design

--- src/Components/intro/intro.jsx
+++ src/Components/intro/intro.jsx
@@ -10,14 +10,14 @@ const Intro = () => {
 
                     <div className="i-title">
                         <div className ="i-title-wrapper">
-                            <div className="i-title-item">Something</div>
-                            <div className="i-title-item">Something else</div>
-                            <div className="i-title-item">More</div>
+                            <div className="i-title-item">X-Ray Analysis</div>
+                            <div className="i-title-item">Medical Library</div>
+                            <div className="i-title-item">Support</div>
                         </div>
                     </div>
 
                     <p className="i-desc">
-                    Text
+                    Our goal is to make medical diagnosis a faster, more efficient and cost-effective process for everyone. Join us in our mission to improve healthcare and change the way we detect and diagnose diseases.
                     </p>
 
                 </div>
