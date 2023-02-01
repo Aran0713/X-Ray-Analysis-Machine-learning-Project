c2eda194cef73f83dda2166f5cae046bbb9a1bb6
EthanKunce <ethankunce@ymail.com>
2023-02-01 08:31:57 +0000

    first working prototype, next is disease description and production

--- MachineLearning/MovingFilesto.py
+++ MachineLearning/MovingFilesto.py
@@ -1,22 +1,25 @@
+#!/usr/bin/python
 import pandas as pd
 import os
 import shutil
+import string as string
 
 df = pd.read_csv('./Data_Entry_2017.csv')
 
 # Runs through excel sheet
 for index, row in df.iterrows():
-    file_name = row['Image Index']
-    category = row['Finding Labels']
-    
-    if os.path.exists('C:/Users/Aran A/Desktop/Hackathon/images_012/images/' + file_name):
-    
-        # Create a new folder for the category if it doesn't already exist
-        if not os.path.exists('C:/Users/Aran A/Desktop/Hackathon/Diseases/'+category):
-            os.makedirs('C:/Users/Aran A/Desktop/Hackathon/Diseases/'+category)
-    
-    # Move the file to the category folder
-        os.rename('C:/Users/Aran A/Desktop/Hackathon/images_012/images/' + file_name,  'C:/Users/Aran A/Desktop/Hackathon/Diseases/'+category+'/'+ file_name)
+    for x in range(13):
+        imageDirectory =  './images_0' if x > 9 else './images_00'
+        imageDirectory = imageDirectory + str(x)+'/images/'
+        file_name = row['Image Index']
+        category = row['Finding Labels']
+        if os.path.exists(imageDirectory + file_name):
+            # Create a new folder for the category if it doesn't already exist
+            category = category.replace("|", " and ")
+            if not os.path.exists('./Diseases/'+category):
+                os.makedirs('./Diseases/'+category)
+        # Move the file to the category folder
+            os.rename(imageDirectory + file_name,  './Diseases/'+category+'/'+ file_name)
 
 
 print("All files have been moved to their respective category folders.")
--- flaskTest/hello.py
+++ flaskTest/hello.py
@@ -3,8 +3,15 @@
 import os
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import cv2
+import tensorflow as tf
+# import os
 
 from flask_cors import CORS
+
+
+DataDir = '../../Diseases'
+Categories = [f for f in os.listdir(DataDir) if os.path.isdir(os.path.join(DataDir, f))]
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -13,6 +20,15 @@ app = Flask(__name__)
 CORS(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+#function that applys filter to user submitted photo -> ML
+def prepare(filepath):
+    IMG_SIZE = 250
+    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    img_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+    img_array = img_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    return img_array
+
+#function that checks if file is legal type -> backend
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -35,12 +51,18 @@ def upload_file():
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
+            directoryAndFile = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(directoryAndFile)
+            model = tf.keras.models.load_model("X-Ray-Analysis.h5")
+            print(model)
+            prediction = model.predict([prepare(directoryAndFile)])
+            print(prediction)
+            diagnosis = Categories[int(prediction[0][0])]
+            print(diagnosis)
             #insert image processing function here.
 
             #test diagnosis
-            diagnosis = "Atelectasis and Cardiomegaly and Consolidation and Edema and Effusion and Infiltration and Mass and Nodule"
+            # diagnosis = "Atelectasis and Cardiomegaly and Consolidation and Edema and Effusion and Infiltration and Mass and Nodule"
             diagnosis = diagnosis.split(" and ")
             # print(diagnosis);
 
